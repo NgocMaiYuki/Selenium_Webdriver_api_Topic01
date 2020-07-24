b5182b0f1358e446a30cfa14d5c4c7a5be23301b
NgocMai <ngocmaijay@gmail.com>
2020-07-24 00:31:15 -0700

    Add new project

---
+++ Webdriver/Selenium_Webdriver_Api/Topic_01_Check_Enviroment.java
@@ -0,0 +1,15 @@
+package Selenium_Webdriver_Api;
+
+import org.testng.annotations.Test;
+
+public class Topic_01_Check_Enviroment {
+	public static void main(String[] args) {
+		System.out.println("Learn to Automation Test");
+
+	}
+
+	@Test
+	public void TC_01() {
+
+	}
+}
